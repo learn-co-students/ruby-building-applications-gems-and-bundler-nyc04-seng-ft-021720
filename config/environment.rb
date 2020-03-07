0d4e03376cecbc1d1f444894c96a3b30c9297e12
Ryan Seit <ryan@ryanseit.com>
2020-03-07 15:25:09 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,9 @@
 # Set up Bundler here following the instructions in the README
+# tells app to use bundler to install gems, required by Gemfile
+require 'bundler/setup'
+
+# specify which bundler groups we want to load
+## load anything no explicitly in a group ':default'
+### load development group ':development'
+#### '.require' means arguments must be passed in the correct order
+Bundler.require(:default, :development)
